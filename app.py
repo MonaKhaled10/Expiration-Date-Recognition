2540d00b5d36c08583d811ef2183fc06a00be2d8
MonaKhaled10 <126685446+MonaKhaled10@users.noreply.github.com>
2025-05-09 20:55:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,11 +59,12 @@ def initialize_models():
         
         # Initialize PaddleOCR
         paddle_ocr = PaddleOCR(
-            use_angle_cls=True,
+            use_angle_cls=False,
             lang='en',
             det_db_score_mode="slow",
             det_db_unclip_ratio=2.0,
-            use_gpu=True,  
+            use_gpu=False,
+            enable_mkldnn=True,
             cpu_threads=6 if not use_gpu else None  # Reduce threads for CPU
         )
         
